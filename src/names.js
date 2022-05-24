347c85a1dcb055dd42cbc6302386ab530d65a2ec
gcid12 <gcid12@gmail.com>
2022-05-24 15:46:54 -0700

    add traits

---
+++ src/names.js
@@ -0,0 +1,36 @@
+export const adjectives = [
+  'fast',
+  'Furious',
+  'Quick',
+  'Emotional',
+  
+];
+
+
+export const roles = [
+  'Punk',
+  'Rock',
+  'Dark',
+  'Degen',
+
+];
+
+
+export const maleNames = [
+  'Jenny',
+  'Tina',
+  'Alex',
+  'Claudia',
+  'Laura'
+];
+
+export const femaleNames = [
+  'Josh',
+  'Rob',
+  'Alex',
+  'Sid',
+  'Tom'
+];
+
+
+
